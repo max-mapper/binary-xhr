df95a497216b1b93c6536a9aeffbc5914637d27a
Recursive Lambda God <chris@circle.academy>
2017-04-29 15:39:52 -0400

    "Recursive Lambda Refactor V1"

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var test = require('tape')
 var bxhr = require('./')
 
-test('basic GET', function(t) {
-  bxhr('https://commondatastorage.googleapis.com/voxeltextures/brick.png', function(err, data) {
+test('basic GET', t => {
+  bxhr('https://commondatastorage.googleapis.com/voxeltextures/brick.png', (err, data) => {
     t.false(err)
     t.true(data instanceof ArrayBuffer)
     t.true(data.byteLength > 0)
